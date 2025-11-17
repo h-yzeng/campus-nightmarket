ef7a6c703315c71a23dec8e2fae6a64fba654f47
Henry Zeng <thyzeng@gmail.com>
2025-11-16 21:29:07 -0600

    Setup basic UI background properties, created empty core components, and setup up types for future use.

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export interface FoodItem {
+  id: number;
+  name: string;
+  seller: string;
+  price: number;
+  image: string;
+  location: string;
+  rating: string;
+  description: string;
+}
+
+export interface CartItem extends FoodItem {
+  quantity: number;
+}
+
+export interface ProfileData {
+  firstName: string;
+  lastName: string;
+  studentId: string;
+  bio: string;
+  photo: string | null;
+}
+
+export type OrderType = 'purchase' | 'pickup' | '';
+
+export type PageType = 'welcome' | 'signup' | 'browse' | 'cart' | 'checkout';
